4c3c4ef05cec255d0346a21ad13dec5199bfe856
Mateusz Jankowski <mat.jankowski99@gmail.com>
2023-12-25 15:50:07 +0100

    Dodaj pliki projektów.

---
+++ WeatherAppWPF/ViewModel/Helpers/AccuWeatherHelper.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherAppWPF.Model;
+
+namespace WeatherAppWPF.ViewModel.Helpers
+{
+    public class AccuWeatherHelper
+    {
+        public const string BASE_URL = "http://dataservice.accuweather.com/";
+        //zwrócić uwagę na te {} => placeholdery
+        public const string AUTOCOMPLETE_ENDPOINT = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
+        public const string CURRENT_CONDITIONS_ENDPOINT = "currentconditions/v1/{0}?apikey={1}";
+        public const string API_KEY = "RHEaGKDdBlcw50ep0TWXQlCCscG30efP";
+
+        public static async Task<List<City>> GetCities(string query)
+        {
+            List<City> cities = new List<City>();
+
+            //string.Format zastępuje placeholdery stringami
+            string url = BASE_URL + string.Format(AUTOCOMPLETE_ENDPOINT, API_KEY, query);
+
+            //request
+            using(HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+                //otrzymuje pewnego jsona z listą miast
+                cities = JsonConvert.DeserializeObject<List<City>>(json);
+            }
+
+            return cities;
+        }
+        
+        public static async Task<CurrentConditions> GetCurrentConditionsAsync(string cityKey)
+        {
+            CurrentConditions currentConditions = new CurrentConditions();
+            string url = BASE_URL + string.Format(CURRENT_CONDITIONS_ENDPOINT, cityKey, API_KEY);
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                string json = await response.Content.ReadAsStringAsync();
+                //otrzymuje pewnego jsona z curr conditions, a przypisuje go do 1 obiektu -> muszę wyciągnąć FirstOrDefault
+                currentConditions = (JsonConvert.DeserializeObject<List<CurrentConditions>>(json)).FirstOrDefault();
+            }
+
+            return currentConditions;
+        }
+
+    }
+}
---
+++ WeatherAppWPF/ViewModel/WeatherVM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAppWPF.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        //które właściwości chciałbym obserwowac?? -> propfull snippet
+        //chciałbym obserwować textboxa do którego będzie wpisywało się miasto
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set 
+            {
+                //ogrywam getterem i setterem pole query, ale przy setterze daje znać, że właściwość obserwowana się zmieniła
+               query = value;
+               OnPropertyChanged("Query");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        //konwencja jest taka, że tworzy się taką metodę
+        //będzie ona przyjmowała nazwę właściwości (tej klasy) jako argument
+        private void OnPropertyChanged(string propertyName)
+        {
+            //gdy obserwowana właściwość się zmieniła invokuje się to
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
